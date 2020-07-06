d6060a33d61139afc0456e029f4c676ee27d1bb7
michaelScarfi <michael.scarfi@gmail.com>
2020-07-06 01:06:06 -0400

    Update sundance.py
    New Raids can be created and written to DB.  Still need to add ability to join and maybe make the creation more robust to actually pull data from DB.

--- sundance.py
+++ sundance.py
@@ -23,12 +23,16 @@ mycursor = mydb.cursor()
 BotToken = os.getenv('BOT_TOKEN')
 ServerToken = os.getenv('SERVER_TOKEN')
 
+sun_chan_code = 683409608987115740
+raid_chan_code = None
+
+raid_setup_active = False
+raid_setup_step = "what"
+raid_setup_id = ""
+
 #create bot object
 #client = discord.Client()
 bot = commands.Bot(command_prefix='!')
-
-sun_chan = None
-raid_chan = None
 
 
 
@@ -42,6 +46,16 @@ async def on_ready():
         f'{bot.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})\n'
     )
+    global sun_chan_code
+    global mycursor
+
+    sun_chanel = bot.get_channel(sun_chan_code)
+    mycursor.execute("SELECT * FROM raid_info")
+    response = 'here are the raids \n' 
+    raid_string = mycursor.fetchall()
+    await sun_chanel.send(response + str(raid_string))
+
+
 
 @bot.event
 async def on_raw_reaction_add(reaction):
@@ -55,15 +69,74 @@ async def on_reaction_add(reaction, user):
     response = f'@{user} added a reaction!'
     await sun_chan.send(response)
 
+@bot.event
+async def on_message(message):
+    global raid_setup_active
+    global raid_setup_user
+    global raid_setup_step
+    global rs_message
+    global mycursor
+    global mydb
+    global raid_setup_id
+
+    if(raid_setup_active):
+        if message.channel.type is discord.ChannelType.private and message.author == raid_setup_user:
+            if(raid_setup_step == "what"):
+                await rs_message.edit(content = f'{rs_message.content} {message.content}')
+                await raid_setup_user.dm_channel.send(f'when?')
+
+                raid_setup_step = "when"
+
+                sql = "UPDATE raid_plan SET what = %s WHERE idRaids = %s"
+                val = (f'{message.content}', raid_setup_id)
+                mycursor.execute(sql, val)
+
+            elif(raid_setup_step == "when"):
+                await rs_message.edit(content = f'{rs_message.content} at {message.content}')
+                await raid_setup_user.dm_channel.send(f'raid setup complete')
+
+                raid_setup_active = False
+                raid_setup_step = "what"
+
+                sql = "UPDATE raid_plan SET time = %s WHERE idRaids = %s"
+                val = (f'{message.content}', raid_setup_id)
+                mycursor.execute(sql, val)
+
+            mydb.commit()
+                
+    await bot.process_commands(message)
+
 @bot.command(name='raid', help='I want to raid')
-async def raid(ctx, what, when):
-    sun_chan = bot.get_channel(683409608987115740)
-    response = f'let\'s go raid {what} at {when}'
-    await sun_chan.send(response)
+async def raid(ctx):
+    global sun_chan_code
+    global raid_setup_active
+    global raid_setup_user 
+    global rs_message
+    global mycursor
+    global mydb
 
+    global raid_setup_id
 
+    raid_setup_active = True
+    raid_setup_user = ctx.message.author
 
-        
+    sun_chan = bot.get_channel(sun_chan_code)
+    response = f'let\'s raid'
+    message = await sun_chan.send(response)
+    rs_message = message
+
+    await raid_setup_user.create_dm()
+    await raid_setup_user.dm_channel.send(f'What raid?')
+
+    sql = "INSERT INTO raid_plan (message_id) VALUE (%s)"
+    val = (message.id,)
+    mycursor.execute(sql, val)
+    mydb.commit()
+
+    raid_setup_id = mycursor.lastrowid
+    
+ 
+
 
 
 
