38e7f9dc7b4a97d6b318416809e4a43716df82c8
michaelScarfi <michael.scarfi@gmail.com>
2020-07-26 23:05:19 -0400

    Update server.py
    improvements to server.py

--- REST_Server/server.py
+++ REST_Server/server.py
@@ -12,30 +12,25 @@ bot_oauth = os.getenv('DESTINY_OATH_CLIENT_ID')
 # Create the application instance
 app = Flask(__name__, template_folder="templates")
 authorization_url = f'https://www.bungie.net/en/OAuth/Authorize?client_id={bot_oauth}&response_type=code'
+# &state=33443
 
 # Create a URL route in our application for "/"
 @app.route('/', methods=['GET'])
 def home():
-    """
-    This function just responds to the browser ULR
-    localhost:5000/
-
-    :return:        the rendered template 'home.html'
-    """
+  
     return render_template('home.html')
 
-@app.route('/api/v1/oath', methods=['GET'])
+@app.route('/api/v1/oauth', methods=['GET'])
 def api_oath():
     # Check if an ID was provided as part of the URL.
     # If ID is provided, assign it to a variable.
     # If no ID is provided, display an error in the browser.
     if 'code' in request.args:
         code = request.args['code']
-        print(code)
+        print(f'User auth code: {code}')
     else:
         return "Error: No id field provided. Please specify an id."
 
-    print(request.args)
 
     # Use the jsonify function from Flask to convert our list of
     # Python dictionaries to the JSON format.
