876facc2d6dc3a9f574a41a4b69f670930b616f0
michaelScarfi <michael.scarfi@gmail.com>
2020-08-07 19:13:40 -0400

    Update admin_cogs.py
    added *notation to expand lists passed to decorators.

--- cogs/admin_cogs.py
+++ cogs/admin_cogs.py
@@ -31,7 +31,7 @@ class admin_cogs(commands.Cog, name='Admin Commands'):
 
     # this is a utility command to refresh a raid post based on data in MySQL DB
     @commands.command(name='refresh', help='type ~refresh and the raid info will be refreshed')
-    @commands.has_any_role(admin_role_codes)
+    @commands.has_any_role(*admin_role_codes)
     async def refresh(self, ctx, raid_id: int):
         # call utility
         await helpers.print_raid(raid_id, ctx.guild.id)
@@ -41,7 +41,7 @@ class admin_cogs(commands.Cog, name='Admin Commands'):
 
     # this command allows a user with certain privileges to delete Raids
     @commands.command(name='delete', help='type ~delete #, this command is only available to admin users.')
-    @commands.has_any_role(admin_role_codes)
+    @commands.has_any_role(*admin_role_codes)
     async def delete(self, ctx, raid_id: int):
         # call utility
         await helpers.delete_raid(raid_id, ctx.guild.id)
@@ -51,7 +51,7 @@ class admin_cogs(commands.Cog, name='Admin Commands'):
 
     # this command allows an admin user to add someone to a raid post
     @commands.command(name='add', help='type add @usertag # #, where # # is the raid ID followed by the spot to add them to that raid.')
-    @commands.has_any_role(admin_role_codes)
+    @commands.has_any_role(*admin_role_codes)
     async def add(self, ctx, user: discord.Member, raid_id: int, spot_id: int):
         # call add user utility
         await helpers.add_user_to_raid(user, raid_id, ctx.guild.id, ctx.message.author, spot_id)
@@ -61,7 +61,7 @@ class admin_cogs(commands.Cog, name='Admin Commands'):
 
     # this command allows an admin user to remove someone from a raid post
     @commands.command(name='remove', help='type remove @usertag #, where # is the raid ID to remove the tagged user from the raid')
-    @commands.has_any_role(admin_role_codes)
+    @commands.has_any_role(*admin_role_codes)
     async def remove(self, ctx, user: discord.Member, raid_id: int):
         # call utility
         await helpers.remove_user(user, raid_id, ctx.guild.id, ctx.message.author)
@@ -71,7 +71,7 @@ class admin_cogs(commands.Cog, name='Admin Commands'):
 
     # this command allows an admin user to reschedule a raid
     @commands.command(name='reschedule', hidden = True)
-    @commands.has_any_role(admin_role_codes)
+    @commands.has_any_role(*admin_role_codes)
     async def reschedule(self, ctx, raid_id: int, new_time: str):
         # call utility to change time
         await helpers.change_raid_time(ctx.message.author, raid_id, ctx.guild.id, new_time)
