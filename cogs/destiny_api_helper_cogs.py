3bd50bca04d40269eddbd089ad0fcda15f3f1f5a
michaelScarfi <michael.scarfi@gmail.com>
2020-08-19 20:16:03 -0400

    Formatting

--- cogs/destiny_api_helper_cogs.py
+++ cogs/destiny_api_helper_cogs.py
@@ -1326,12 +1326,12 @@ class destiny_api_helper_cogs(commands.Cog, name='Destiny Utilities'):
             for name in combo_df.iloc[i]['names']:
                 names_message += f'{name}\n'
 
-            DIM_search = ""
+            DIM_search = "`"
             for i, Id in enumerate(combo_df.iloc[i]['ids']):
                 if i != 3:
-                    DIM_search += f'id={Id} or'
+                    DIM_search += f'id:{Id} or '
                 else:
-                    DIM_search += f'id={Id}'
+                    DIM_search += f'id:{Id}`'
 
             # create message showing base stat values
             base_stats_message = ""
@@ -1371,7 +1371,7 @@ class destiny_api_helper_cogs(commands.Cog, name='Destiny Utilities'):
             base_stats_message += f'Extra Points: {extra_points}'
 
             # add to embed
-            embed.add_field(name=f'Armor Set {i+1}:', value=f'Total Tiers: {final_stat_tiers}\nDIM Search String: {DIM_search}', inline = False)
+            embed.add_field(name=f'Armor Set {i+1}:', value=f'Total Tiers: {final_stat_tiers}\n**DIM Search String**: {DIM_search}', inline = False)
             embed.add_field(name=f'Armor Pieces:', value = names_message, inline = True)
             embed.add_field(name=f'Base Stats:', value = base_stats_message, inline = True)
             embed.add_field(name=f'Final Stats:', value = final_stats_message, inline = True)
