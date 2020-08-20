68ac58e4d829d0f6033e6b88a63d456aab454a78
michaelScarfi <michael.scarfi@gmail.com>
2020-08-19 21:09:20 -0400

    Reduced limits

--- cogs/destiny_api_helper_cogs.py
+++ cogs/destiny_api_helper_cogs.py
@@ -1063,16 +1063,16 @@ class destiny_api_helper_cogs(commands.Cog, name='Destiny Utilities'):
             calc_item_df['trait3_score'] = trait3_scores
 
             # remove all items that will decrease score/cannot results in score increase
-            if(len(calc_item_df.index) > 60):
+            if(len(calc_item_df.index) > 50):
                 calc_item_df = calc_item_df[calc_item_df.primary_score >= 0]
                 calc_item_df = calc_item_df.reset_index(drop=True)
             # remove all items that result in a reduction in potential tiers if we have too many items, we can now potentially decrease surplus given highest_primary_score.
             #if(len(temp_item_df.index) > 75):
             #    temp_item_df = temp_item_df[temp_item_df.cost <= (surplus)]
             #    temp_item_df = temp_item_df.reset_index(drop=True)
-            if(len(calc_item_df.index) > 75):
+            if(len(calc_item_df.index) > 60):
                 calc_item_df = calc_item_df.sort_values(by=['primary_score','trait3_score','cost'], ascending=[False, False, True])
-                calc_item_df = calc_item_df.head(75)
+                calc_item_df = calc_item_df.head(60)
 
         # create list of high_item ids
         high_item_ids = []
