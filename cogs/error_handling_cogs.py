3262898863c8849a583eadc14758e088cebf36d3
michaelScarfi <michael.scarfi@gmail.com>
2020-08-23 00:04:41 -0400

    Chained message to string instead of list

--- cogs/error_handling_cogs.py
+++ cogs/error_handling_cogs.py
@@ -104,7 +104,7 @@ class error_handling_cogs(commands.Cog):
             try:
                 raise error
             except Exception as err:
-                message = traceback.format_exception(None, err, err.__traceback__, limit=None, chain=True)
+                message = "".join(traceback.format_exception(None, err, err.__traceback__, limit=None, chain=True))
                 client.report(message, user = str(ctx.message.author.id))
             # delete command message to keep channels clean if not a dm and bot has permissions
             if ctx.channel.type is ChannelType.text and ctx.guild.me.guild_permissions.manage_messages:
