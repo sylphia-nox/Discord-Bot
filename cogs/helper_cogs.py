305adeaaad42be070d8db4bb56bdf50a41c36d9f
michaelScarfi <michael.scarfi@gmail.com>
2020-09-10 23:18:20 -0400

    Fix to DB purging.

--- cogs/helper_cogs.py
+++ cogs/helper_cogs.py
@@ -443,7 +443,7 @@ class helper_cogs(commands.Cog, name='Utilities'):
             for owner in oauth_owners:
                 if not int(owner) in member_ids:
                     bad_items += 1
-                    await self.write_db("DELETE FROM `oauth_tokens` WHERE `discordID` = '%s'", [member.id,])
+                    await self.write_db("DELETE FROM `oauth_tokens` WHERE `discordID` = '%s'", [owner.id,])
 
         if (bad_items > 0):
             print(f'Removed {bad_items} bad entries from db')
